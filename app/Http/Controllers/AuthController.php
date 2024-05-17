8edb4a2e0162d2b30bdf50137e77e41e3fd9abfd
Joshigo <hidalgojosemanuel59@gmail.com>
2024-05-17 17:40:45 -0400

    changes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function registro(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = new User([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        $user->save();
+
+        return response()->json(['message' => 'Usuario registrado con éxito'], 201);
+    }
+
+    public function login(Request $request){
+        $validator = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'errors' => [
+                    'msg' => ['*Los datos ingresados son incorrectos'],
+                ],
+            ],  422);
+        }
+
+        $token = $user->createToken($request->email)->plainTextToken;
+        return response()->json([
+            'res' => true,
+            'token' => $token,
+            'user' => $user
+        ],200);
+    }
+}
