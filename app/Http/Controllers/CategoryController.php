d4310e966f7ee250f0e57c7892f7cc922ce385de
Joshigo <hidalgojosemanuel59@gmail.com>
2024-05-22 12:17:22 -0400

    validation on get status

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::where('status', 1)->get();
         return response()->json($categories);
     }
 
